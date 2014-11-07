b59fcc23c34557f564616814d960cf397592b26c
FlorinGh <florin_gheorghe@ymail.com>
2014-11-07 07:50:33 +0200

    working on quiz, third commit
    Problems 1.1, 1.2, 2, 3

--- Quiz2/Quiz2P2.py
+++ Quiz2/Quiz2P2.py
@@ -1,36 +1,20 @@
-#Quiz 2, Problem 2
+# Quiz 2, Problem 3
 
-# Importing libraries
-import numpy as np
-pi = np.pi
+# Input data
+l = 200.0*1e-6
+b = 20.0*1e-6
+h = 1.0*1e-6
+E = 300.0*1e9
+emax = 0.1*1e-2
 
-# The stress matrix for initial orientation of the element
-# sx is the unknown
-# we will create a function to look for the sx
-sy = 8
-txy = 16
-s0 = 40
+# Geometric properties of the beam
+ymax = h/2.0
+I = (b*h**3)/12.0
 
-# Creating a function to determine the principal stress components
-def S1(sx, sy = 8, txy = 16):
-    smed = 0.5*(sx+sy)
-    R = np.sqrt((0.5*(sx-sy))**2+(txy)**2)
-    s1 = smed + R
-    return s1
+# Determining the maximum P
+smax = E*emax
+Mmax = smax*I/ymax
+Pmax = Mmax/l
 
-# Creating a search function for sx using bisection method
-def searchSx(s):
-    s0 = 40.0
-    eps = 0.01
-    if S1(s) > s0-eps and S1(s) < s0+eps:
-        print 'Sx is ' + str(s)
-    elif S1(s) < s0-eps:
-        #print str(s) + ' is small'
-        s = s + 0.1
-        searchSx(s)
-    else:
-        #print str(s) + ' is big'
-        s = s - 0.1
-        searchSx(s)
-        
-searchSx(40)
+# Printing the solution, force in microN
+print round(Pmax*1e6, 5)
--- Quiz2/Quiz2P3.py
+++ Quiz2/Quiz2P3.py
@@ -1,20 +1,12 @@
 # Quiz 2, Problem 3
 
-# Input data
-l = 200.0*1e-6
-b = 20.0*1e-6
-h = 1.0*1e-6
-E = 300.0*1e9
-emax = 0.1*1e-2
+# Input data: time, stress and creep compliance
+t = [0.0, 25.0, 50.0, 75.0]
+s = [10.0, 30.0, 0.0, 0.0]
+J = [1.0*1e-3, 1.4*1e-3, 1.6*1e-3, 1.7*1e-3]
 
-# Geometric properties of the beam
-ymax = h/2.0
-I = (b*h**3)/12.0
+# Determining the strain at 75 hours
+e75 = s[0]*J[3] + (s[1]-s[0])*J[2] + (s[2]-s[1])*J[1]
 
-# Determining the maximum P
-smax = E*emax
-Mmax = smax*I/ymax
-Pmax = Mmax/l
-
-# Printing the solution, force in microN
-print round(Pmax*1e6, 5)
+# Printing the solution
+print round(e75, 5)
