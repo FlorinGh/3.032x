e9a4346f450a202ec830865d8c7d291498e63a5e
FlorinGh <florin_gheorghe@ymail.com>
2014-11-15 18:53:02 +0200

    Week 9
    Finished the HW

--- Week9/Week9P3.py
+++ Week9/Week9P3.py
@@ -3,42 +3,21 @@
 
 import numpy as np
 
-# Problem 4
+# Problem 3
 """
-An aluminum alloy has a yield strangth σy=150MPa. A component made from this alloy
-is subjected to σx=200MPa and σy=50MPa, with all other stresses equal to zero.
+The lattice for calcium is FCC, and the radius, r, of a calcium atom is 197pm.
+Assuming that the dislocation density ρ is 5×1e8 cm−2 and that it remains constant,
+and the plastic shear strain rate γ˙ is 8×1e−5 s−1, what is the mean velocity of
+the dislocation? Note that for an FCC crystal, the Burgers vector is equal to 2r.
 """
 
-# Input data (the complete stress matrix)
-sx = 200.0
-sy = 50.0
-sz = 0.0
-sxy = 0.0
-syz = 0.0
-szx = 0.0
+# Input data 
+r = 197.0*1e-12
+rho = 5.0*1e12
+gama = 8.0*1e-5
+b = 2*r
 
-Sy = 150.0
+# Computing the velocity of dislocation
+v = gama/(b*rho)
+print 'Velocity dislocation is ' + str(v)
 
-
-# Computing the principal stress components
-smed = 0.5*(sx + sy)
-R = np.sqrt((0.5*sx - 0.5*sy)**2 + sxy**2)
-s1 = smed + R
-print 's1 is ' + str(round(s1,4))
-s2 = smed - R
-print 's2 is ' + str(round(s2,4))
-s3 = 0.0
-print 's3 is ' + str(round(s3,4))
-
-# Calculate the difference between the maximum and minimum principal stresses.
-print (s1-s3)
-
-# Does the component fail according to the Tresca criterion?
-print (s1-s3) > Sy
-
-# Calculate the equivalent stress using principal stress components
-seq = np.sqrt(s1**2 + s2**2 + s3**2 - s1*s2 - s2*s3 - s3*s1)
-print 'seq is ' + str(round(seq,4))
-
-# Does the component fail according to the von Mises criterion?
-print seq > Sy
